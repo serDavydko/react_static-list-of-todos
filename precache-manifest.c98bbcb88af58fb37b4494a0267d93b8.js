b1b9016d1d6675ae7515ec704fa18d4ed3319946
Serhii <davydko.serhiy@gmail.com>
2019-07-08 14:56:33 +0300

    Updates

---
+++ precache-manifest.c98bbcb88af58fb37b4494a0267d93b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7def3eb93eaf4a8e2c3935281c5cde22",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "4b6a29650c04c5a000e5",
+    "url": "/react_static-list-of-todos/static/css/main.43c17014.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-todos/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "4b6a29650c04c5a000e5",
+    "url": "/react_static-list-of-todos/static/js/main.a46b9f40.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
